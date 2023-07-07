785fbc0dd0164ddf18bbfff35972c8a8788ad2c2
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-07-07 09:22:31 +0900

    update

--- script/drop.js
+++ script/drop.js
@@ -1,10 +1,11 @@
-document.querySelector('.read-more').addEventListener('click', function() {
-    var content = this.parentNode;
-    content.classList.toggle('show-more');
-    if (content.classList.contains('show-more')) {
-      this.innerText = 'Read less';
-    } else {
-      this.innerText = 'Read more';
+document.addEventListener('DOMContentLoaded', function() {
+    const foldableButtons = document.querySelectorAll('.foldable-button');
+    const foldableContents = document.querySelectorAll('.foldable-content');
+  
+    for (let i = 0; i < foldableButtons.length; i++) {
+      foldableButtons[i].addEventListener('click', function() {
+        foldableContents[i].classList.toggle('open');
+      });
     }
   });
   
