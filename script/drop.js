0304f3ab7575ff72394ce2fdd59582da430f0f35
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-07-07 09:05:49 +0900

    update

---
+++ script/drop.js
@@ -0,0 +1,10 @@
+document.querySelector('.read-more').addEventListener('click', function() {
+    var content = this.parentNode;
+    content.classList.toggle('show-more');
+    if (content.classList.contains('show-more')) {
+      this.innerText = 'Read less';
+    } else {
+      this.innerText = 'Read more';
+    }
+  });
+  
